b8b3d9fef7ef8020f17ffa80efe21233edbf0460
Joey Chen <jay.hatelove@gmail.com>
2022-10-09 00:10:05 +0800

    refactor: add __ name convention for some function as private functions

--- budget_service_tests.py
+++ budget_service_tests.py
@@ -8,10 +8,10 @@ from budget_service import Budget, BudgetService
 
 class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
-        get_budgets_patcher = patch('budget_service.BudgetService.get_budgets')
+        get_budgets_patcher = patch('budget_service.BudgetService._BudgetService__get_budgets')
         self.fake_get_budgets = get_budgets_patcher.start()
         self.budget_service = BudgetService()
-        self.fake_get_budgets.return_value = [Budget("202210", 3100)]
+        # self.fake_get_budgets.return_value = [Budget("202210", 3100)]
 
     # def test_should_get_full_month_budget_private(self):
     #     test_date = datetime(2022, 10, 2)
